fbdc974467ff12b98bfb587ce2ec5b4c2a579dd0
Sintez7 <berzzzurgg@gmail.com>
2020-08-17 13:15:10 +0300

    add new project

---
+++ Java1/BShips_v1.0/src/app/Controller.java
@@ -0,0 +1,14 @@
+package app;
+
+import javafx.stage.Stage;
+
+public class Controller {
+
+    private Main main;
+    private Stage stage;
+
+    public Controller(Main main, Stage primaryStage) {
+        this.main = main;
+        stage = primaryStage;
+    }
+}
---
+++ Java1/BShips_v1.0/src/app/Main.java
@@ -0,0 +1,27 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        Controller controller = new Controller(this, primaryStage);
+        loader.setLocation(getClass().getResource("sample.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
+        primaryStage.setTitle("Battle Ships The Game");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
