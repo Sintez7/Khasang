0469d474f455cb57aebe618e752945374de467e7
Sintez7 <berzzzurgg@gmail.com>
2021-01-12 18:17:01 +0300

    work 12 01 day

--- Java1/BShips_Server_v1.0/src/app/shared/EnterRoom.java
+++ Java1/BShips_Server_v1.0/src/app/shared/EnterRoom.java
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 package app.shared;
 
 public class EnterRoom extends DataPackage {
