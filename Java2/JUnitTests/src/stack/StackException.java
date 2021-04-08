d06578df9df4d2062b15c38a40eae6d67ea1911e
Deusolbert <berzzzurgg@gmail.com>
2021-04-08 18:45:41 +0300

    work 08 04 day

---
+++ Java2/JUnitTests/src/stack/StackException.java
@@ -0,0 +1,14 @@
+package stack;
+public class StackException extends Exception{
+
+    private final String reason;
+
+    public StackException(String reason) {
+        this.reason = reason;
+    }
+
+    @Override
+    public String toString() {
+        return "StackException reason: " + reason;
+    }
+}
